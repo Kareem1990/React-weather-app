5c57365e3d4446c748bf7ab6eb4a0e1dbd6663f8
Kareem Rizk <kareem.magdy5@gmail.com>
2022-08-05 11:31:41 -0400

    fetched lat and long apis

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
+export default function App() {
+  
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
-const [ lat, setLat ] = useState([]);
-const [long, setLong ] = useState([]);
-useEffect(() => {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    setLat(position.coords.latitude);
-    setLong(position.coords.longitude);
-  });
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-  console.log("Latitude is:", lat)
-  console.log("Longitude is:", long)
-}, [lat, long]);
-
-function App() {
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
-
-export default App;
